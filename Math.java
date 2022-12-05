875ec02a6ea4b2b4429b46402e9482ff03b0f46b
Smeetbdoshi <118739022+Smeetbdoshi@users.noreply.github.com>
2022-12-05 13:40:29 +0000

    Update Math.java

--- Math.java
+++ Math.java
@@ -12,4 +12,14 @@ a= kin.nextInt();
 b= kin.nextInt();
 System.out.println("c="+a+b);
 }
-
+/****
+Smeet Doshi****/
+public void Modtest{
+int a=0;
+int b=0;
+System.out.println();
+Scanner kin=new Scanner(System.in);
+a= kin.nextInt();
+b= kin.nextInt();
+System.out.println("c="+a%b);
+}
