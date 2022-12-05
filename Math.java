0fdbde938c590f2222622f7fd17f71694cf9492a
Sabira Memon <s.memon@aston.ac.uk>
2022-12-05 12:50:14 +0000

    sum test

--- Math.java
+++ Math.java
@@ -1,2 +1,15 @@
 /*****
 akhil jasti***/
+/******
+Sabira Memon***/
+id no= 210223899
+public void Sumtest{
+int a=0;
+int b=0;
+System.out.println();
+Scanner kin=new Scanner(System.in);
+a= kin.nextInt();
+b= kin.nextInt();
+System.out.println("c="+a+b);
+}
+
